517c95f8bba968cc66063553645252a032874702
adityaputravu <32431279+adityaputravu@users.noreply.github.com>
2019-05-29 18:21:21 +0100

    Add files via upload

---
+++ ReversingELF/BasicRE/clue_generating_flag.py
@@ -0,0 +1,15 @@
+#flag = HIDDEN
+flag = bytearray(map(ord,flag))
+
+def bitflip(num):
+    num = bin(num)[2:]
+
+    return new
+
+new  = []
+for c in flag:
+    new.append(c^0xff)
+
+new_new = list(map(hex, new))
+
+print(flag, new, len(new))
