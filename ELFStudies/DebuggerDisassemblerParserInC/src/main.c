48f46afa48490ed816d86bac0cb97adfc777d742
adityaputravu <aputravu1234@gmail.com>
2019-11-18 22:16:36 +0000

    message

--- ELFStudies/DebuggerDisassemblerParserInC/src/main.c
+++ ELFStudies/DebuggerDisassemblerParserInC/src/main.c
@@ -8,10 +8,10 @@
 
 int main(int argc, char* argv[]){
     if(argc >= 2){
-	//attach(argv[1], NULL); 
 	struct e_header64* header;
 	header = elf64_header_struct(argv[1]);
 	print_elf_header(header);
+	attach(argv[1], NULL); 
     }
     else
 	printf("Usage: %s <file>\n", argv[0]);
