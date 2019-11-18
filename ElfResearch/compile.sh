509fde0ddd27e73ee16d71db7f8322daee85fc08
Aditya Putravu <32431279+adityaputravu@users.noreply.github.com>
2019-11-18 00:09:08 +0000

    Delete compile.sh

--- ElfResearch/compile.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-gcc -c -fpic $PWD/src/header.c -o $PWD/src/header.o && gcc -shared -o $PWD/bin/libheader.so $PWD/src/header.o
-
-gcc -c -fpic $PWD/src/strings.c -o $PWD/src/strings.o && gcc -shared -o $PWD/bin/libstrings.so $PWD/src/strings.o
-
-gcc -c -fpic $PWD/src/debugger.c -o $PWD/src/debugger.o && gcc -shared -o $PWD/bin/libdebugger.so $PWD/src/debugger.o $PWD/src/strings.o
-
-
